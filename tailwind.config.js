0ef81f655441fae38f0e77b541c4fda2295ac05a
octav1an <octav1an@hotmail.com>
2025-01-12 15:19:56 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./client/*.html", "./client/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
