2e3f8a4d9acf1c7e6dbc60a7433da29ed59aefcf
octav1an <octav1an@hotmail.com>
2025-01-13 23:15:52 +0100

    replace tailwind with mui

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./client/*.html", "./client/**/*.{js, jsx, ts, tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
