5be454fde36ce540d5fafe97e4cf9c8511ce850d
octav1an <octav1an@hotmail.com>
2025-02-08 15:00:54 +0100

    update scripts

--- scripts/create_app.sh
+++ scripts/create_app.sh
@@ -2,5 +2,6 @@
 
 source .env
 
+[  -d "${BUILD_DIR}" ] || mkdir "${BUILD_DIR}"
 
-clasp create --title "$PROJECT_NAME" --rootDir ./build
+clasp create --title "$PROJECT_NAME" --rootDir ./build --type webapp
