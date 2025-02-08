a33b1edb2d1882ab7df65ab79a175f90c720a44c
octav1an <octav1an@hotmail.com>
2025-02-08 17:17:41 +0100

    update create script

--- scripts/create_app.sh
+++ scripts/create_app.sh
@@ -5,3 +5,7 @@ source .env
 [  -d "${BUILD_DIR}" ] || mkdir "${BUILD_DIR}"
 
 clasp create --title "$PROJECT_NAME" --rootDir ./build --type webapp
+cp appsscript.json "${BUILD_DIR}"
+
+# Create a symlink to clasp in the project root folder to avoid always calling clasp with -P arg
+ln -s build/.clasp.json .clasp.json
